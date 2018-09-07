610fc62976e511631a2a2fd6b8325cd730a03adc
SepityAJ@rbtechnologies.ru <SepityAJ@rbtechnologies.ru>
2018-09-07 09:30:57 +0300

    1

---
+++ RestCaseNoBase-war/src/main/java/rest/RestApplication.java
@@ -0,0 +1,26 @@
+package rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Конфигурация REST приложения
+ */
+@ApplicationPath("/rest")
+public class RestApplication extends Application {
+    private Set<Object> singletons = new HashSet<Object>();
+    private Set<Class<?>> empty = new HashSet<Class<?>>();
+    public RestApplication(){
+//        singletons.add(new RestResource());
+    }
+    @Override
+    public Set<Class<?>> getClasses() {
+        return empty;
+    }
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
---
+++ tree.cmd
@@ -0,0 +1,4 @@
+set JAVA_HOME=c:\java\jdk8
+set M2_HOME=c:\java\mvn311
+set path=%JAVA_HOME%\bin;%M2_HOME%\bin;%path%
+c:\java\mvn311\bin\mvn dependecy:tree
